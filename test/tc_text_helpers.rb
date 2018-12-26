e85ec22e186d40d8db3594d9fa982f861a2a55d7
Patrick Reynolds <patrick.reynolds@github.com>
2018-12-26 16:40:31 -0500

    Combine simple test suites

--- test/tc_text_helpers.rb
+++ test/tc_text_helpers.rb
@@ -4,7 +4,7 @@ require 'test/unit'
 require '../lib/movie'
 require '../lib/parser'
 
-class TestGetCast < Test::Unit::TestCase
+class TestTextHelpers < Test::Unit::TestCase
 	def test_get_cast
 		assert_equal([], Movie.get_cast("==Cast==\nfoo"))
 		assert_equal([], Movie.get_cast("abcd\n==Cast==\nfoo"))
@@ -13,5 +13,17 @@ class TestGetCast < Test::Unit::TestCase
 		assert_equal([], Movie.get_cast("abcd\n=== Cast {{junk}} ===\nfoo"))
 		assert_equal([], Movie.get_cast("abcd\n==Main cast==\nfoo"))
 		assert_equal(nil, Movie.get_cast("abcd\n==Cat==\nfoo"))
+	end
+
+	def test_plain_textify
+		assert_equal("AfooB", Movie.plain_textify("A{{ill|foo|bar}}B"))
+		assert_equal("AtextB", Movie.plain_textify("A[[target|text]]B"))
+		assert_equal("AfooB CtextD", Movie.plain_textify("A{{ill|foo|bar}}B C[[target|text]]D"))
+	end
+
+	def test_plainlist
+		assert_equal("", Movie.convert_plainlists(""))
+		assert_equal("abc", Movie.convert_plainlists("abc"))
+		assert_equal("XX{{ubl|A|B}}YY", Movie.convert_plainlists("XX{{Plainlist|\n* A\n* B\n}}YY"))
 	end
 end
